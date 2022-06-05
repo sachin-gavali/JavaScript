f4a239e3e2cc67499b06c5633d33ca6a329cad4b
sachin-gavali <98352918+sachin-gavali@users.noreply.github.com>
2022-06-05 07:51:39 +0530

    Add files via upload

---
+++ All_JavaScript.js
@@ -0,0 +1,162 @@
+//console.log(" Sachin Gavali ");
+
+
+// console.log is use for printnting the output
+
+
+// Oprator 
+
+
+
+/* var num1 = 10;
+var num2 = 20;
+var sum = num1 + num2;
+var sub = num1 - num2;
+var multi = num1 * num2
+var div = num1 / num2;
+var mod = num1 % num2;
+console.log(" Num 1 is = " + num1);
+console.log(" Sum  is = " + sum);
+console.log(" Sub  is = " + sub);
+console.log(" multi  is = " + multi);
+console.log(" Div  is = " + div);
+console.log(" Mod   is = " + mod);
+console.log(" Num 2 is = " + num2); */
+
+
+
+
+// if else 
+
+
+
+/* let age = 18;
+if (age < 18) {
+    console.log(" You can't vote ");
+
+} else if (age == 18) {
+    console.log(" Sachin Gavali is best  ");
+
+} else {
+    console.log("You can vote ");
+
+} */
+
+
+
+
+// Switch case  in JavaScript 
+
+
+/* let roll_no = 17;
+switch (roll_no) {
+    case 10:
+        {
+            console.log(" Your numbe is 10");
+        }
+        break;
+    case 11:
+        {
+            console.log(" Your numbe is 11 ");
+        }
+        break;
+    case 12:
+        {
+            console.log(" Your numbe is 12");
+        }
+        break;
+    case 13:
+        {
+            console.log(" Your numbe is 13");
+        }
+        break;
+    case 14:
+        {
+            console.log(" Your numbe is 14");
+        }
+        break;
+    case 15:
+        {
+            console.log(" Your numbe is 15");
+        }
+        break;
+    case 16:
+        {
+            console.log(" Your numbe is 16");
+
+            break;
+        }
+    case 17:
+        {
+            console.log(" Your numbe is 17");
+        }
+        break;
+    case 18:
+        {
+            console.log(" Your numbe is18 ");
+        }
+        break;
+    case 19:
+        {
+            console.log(" Your numbe is 19");
+        }
+        break;
+    case 20:
+        {
+            console.log(" Your numbe is 20");
+        }
+        break;
+
+
+    default:
+        {
+            console.log(" Invald number");
+        }
+        break;
+} */
+
+
+
+// String function in  in JavaScript 
+
+
+/* let str = "Sachin Gavali";
+let str2 = " Hello ";
+console.log(str);
+console.log(str.length);
+console.log(str.indexOf(" Ga"))
+console.log(str.lastIndexOf(" in"));
+
+console.log(str.slice(1, 6));
+console.log(str.substring(1, 6));
+console.log(str.substring(1, 6));
+console.log(str + str2);
+console.log(str.concat(str2)); */
+// Object in JavaScript 
+/* let student = {
+    name: " Sachin Gavali ",
+    roll_no: 19,
+    phone_no: 9067339470,
+    fav_lang: " java",
+    fav_sub: " c programming ",
+};
+console.log(student); */
+
+
+
+// Array in JavaScript
+/* let names = [1, 2, 3];
+console.log(names);
+let Name = new Array(1, 2);
+console.log(Name);
+ */
+
+
+
+/* let names = [45, 46, 24, 23, 65, 76, 4, 34, 56, 3, 353, 35, 6];
+
+console.log(a.length);
+names = names.sort();
+console.log(names);
+names.push(" Sachin Gavali");
+console.log(names); */
